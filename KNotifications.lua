56e88ea1771c0664b288d45019bf949b67da8166
DELETIONSs <officialkadencreations@gmail.com>
2025-01-14 16:59:47 -0500

    Update KNotifications.lua

--- KNotifications.lua
+++ KNotifications.lua
@@ -18,24 +18,24 @@
 
 local K-Stuff = {
   K-Colors = {
-    Red: #FF5449, -- #FF5449
-    Red-Orange: #ff4b33, -- Red-Orange *Figma 2025 (https://www.figma.com/colors/red-orange/)
-    Orange: #,
-    Yellow: #,
-    Green: #6200EE,
-    Blue: #2323ff, -- Neon-Blue *Figma 2025 (https://www.figma.com/colors/neon-blue/)
-    Purple: #6200EE,
-    Black: #0A0A08;
+    Red: Color3.fromRGB(255, 84, 73) -- [#FF5449] | K-Dev
+    Red-Orange: #ff4b33, -- [#ff4b33] | Red-Orange *Figma 2025 (https://www.figma.com/colors/red-orange/)
+    Orange: #, -- [#] | K-Dev
+    Yellow: #, -- [#] | K-Dev
+    Green: #, -- [#] | K-Dev
+    Blue: #2323ff, -- [#2323ff] | Neon-Blue *Figma 2025 (https://www.figma.com/colors/neon-blue/)
+    Purple: #6200EE, -- [#6200EE] | K-Dev 
+    Black: #0A0A08; --[#0A0A08] | K-Dev 
   }
 }
 
 local K-UI-Theme = {
   theme-dark {
-  Main: #000000,
-  Border: #343434,
+  Main: Color3.fromRGB(0,0,0),
+  Border: Color3.fromRGB(52, 52, 52),
   }
   theme-light {
-      Main: #000000,
+  Main: Color3.fromRGB(204,204,214),
   Border: #343434,
   }
 }
