bf1b7a0cd46c2e8db955912f732213283af359e5
DELETIONSs <officialkadencreations@gmail.com>
2025-01-14 16:55:11 -0500

    Create KNotifications.lua

---
+++ KNotifications.lua
@@ -0,0 +1,41 @@
+--[[
+
+>=>   >=>          >====>                           
+>=>  >=>           >=>   >=>                        
+>=> >=>            >=>    >=>   >==>    >=>     >=> 
+>>=>>       >====> >=>    >=> >>   >=>   >=>   >=>  
+>=>  >=>           >=>    >=> >>===>>=>   >=> >=>   
+>=>   >=>          >=>   >=>  >>           >=>=>    
+>=>     >=>        >====>      >====>       >=>     
+
+-- Owner: re_sistance (Kadan6407's Alt)
+-- Scripting: re_sistance
+-- Colors: re_sistance
+-- Language: luau (Roblox Lua) 
+-- Thank you <3
+
+]]
+
+local K-Stuff = {
+  K-Colors = {
+    Red: #FF5449, -- #FF5449
+    Red-Orange: #ff4b33, -- Red-Orange *Figma 2025 (https://www.figma.com/colors/red-orange/)
+    Orange: #,
+    Yellow: #,
+    Green: #6200EE,
+    Blue: #2323ff, -- Neon-Blue *Figma 2025 (https://www.figma.com/colors/neon-blue/)
+    Purple: #6200EE,
+    Black: #0A0A08;
+  }
+}
+
+local K-UI-Theme = {
+  theme-dark {
+  Main: #000000,
+  Border: #343434,
+  }
+  theme-light {
+      Main: #000000,
+  Border: #343434,
+  }
+}
